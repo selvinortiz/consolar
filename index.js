fe144a08c580fcb72dea0033739c0fd8cf941fcb
Selvin Ortiz <selvin@selvin.co>
2015-02-28 17:04:49 -0600

    Consolar 0.3.0 - Adds lodash as an avilable dependency for commands - Adds support for option and argument validation - Adds the ability for commands to run as standalone - Adds the ability to customize usage help - Fixes some issues with initialization - Fixes some issues where the command was not executed properly

--- index.js
+++ index.js
@@ -30,23 +30,26 @@ swig.setFilter('accent', function(input) {
   return chalk.green(input);
 });
 
-var prefixChar  = '-';
-var currentStep = 1;
+var prefixChar     = '-';
+var currentStep    = 1;
+var consoleCommand = '';
 
 // @Definition
 var Consolar = function() {
   this.cmd           = '';
   this.opts          = {};
   this.args          = [];
+  this.namedArgs     = {};
   this.name          = 'Consolar';
   this.version       = '0.1.2';
   this.basePath      = __dirname;
   this.consoleInput  = [];
-  this.commandPrefix = 'consolar';
+  this.commandPrefix = '';
 
   // @Stash
-  this.swig  = swig;
-  this.chalk = chalk;
+  this.swig   = swig;
+  this.chalk  = chalk;
+  this.lodash = lodash;
 };
 
 Consolar.prototype.init = function(config) {
@@ -61,6 +64,10 @@ Consolar.prototype.init = function(config) {
 
   lodash.forEach(args, function(value, key) {
     if (key === '_') {
+      if (self.hasConsoleCommand()) {
+        args._ = [self.getConsoleCommand()].concat(args._);
+      }
+
       lodash.forEach(args._, function(v, k) {
         if (k === 0) {
           self.cmd = v;
@@ -72,6 +79,8 @@ Consolar.prototype.init = function(config) {
       self.opts[lodash.camelCase(key)] = value;
     }
   });
+
+  // console.log([self.cmd, self.opts, self.args, this.shouldShowVersion(), this.shouldShowHelp(), this.shouldShowCommandHelp()]);
 
   if (this.shouldShowVersion()) {
     console.log(this.version);
@@ -91,11 +100,33 @@ Consolar.prototype.init = function(config) {
   this.runCommand();
 };
 
-Consolar.prototype.runCommand = function() {
-  var name = this.commandPrefix + this.normalizeCommand(this.cmd);
+/**
+ * Sets the console command for the current session
+ *
+ * @note
+ * This must be set by self running commands
+ */
+Consolar.prototype.setConsoleCommand = function(cmd) {
+  consoleCommand = cmd;
+};
 
+Consolar.prototype.hasConsoleCommand = function() {
+  return consoleCommand !== '' ? true : false;
+};
+
+Consolar.prototype.getConsoleCommand = function() {
+  return consoleCommand;
+};
+
+Consolar.prototype.runCommand = function() {
   try {
-    var cmd = require(name);
+    var cmd;
+
+    if (this.hasConsoleCommand()) {
+      cmd = require(this.basePath + '/index');
+    } else {
+      cmd = require(this.commandPrefix + this.normalizeCommand(this.cmd));
+    }
 
     cmd.init(this);
     cmd.run();
@@ -103,6 +134,8 @@ Consolar.prototype.runCommand = function() {
   catch(e) {
     if (e.code === 'MODULE_NOT_FOUND') {
       this.halt('The %s command is not supported.', [this.cmd]);
+    } else {
+      this.halt(e.message);
     }
   }
 };
@@ -113,10 +146,14 @@ Consolar.prototype.renderHelp = function(data) {
 };
 
 Consolar.prototype.renderCommandHelp = function(data) {
-  var name = this.commandPrefix + this.normalizeCommand(this.cmd);
-
   try {
-    var cmd = require(name);
+    var cmd;
+
+    if (this.hasConsoleCommand()) {
+      cmd = require(this.basePath + '/index');
+    } else {
+      cmd = require(this.commandPrefix + this.normalizeCommand(this.cmd));
+    }
 
     cmd.init(this);
     cmd.renderHelp();
@@ -124,7 +161,80 @@ Consolar.prototype.renderCommandHelp = function(data) {
   catch(e) {
     if (e.code === 'MODULE_NOT_FOUND') {
       this.halt('The %s command is not supported!', [name]);
+    } else {
+      this.halt(e.message);
     }
+  }
+};
+
+Consolar.prototype.shouldShowVersion = function() {
+  return lodash.has(this.opts, 'v') || lodash.has(this.opts, 'version');
+};
+
+Consolar.prototype.shouldShowHelp = function() {
+  if ((lodash.has(this.opts, 'h') || lodash.has(this.opts, 'help')) && lodash.isEmpty(this.cmd)) {
+    return true;
+  }
+
+  return false;
+};
+
+Consolar.prototype.shouldShowCommandHelp = function() {
+  if ((lodash.has(this.opts, 'h') || lodash.has(this.opts, 'help')) && !lodash.isEmpty(this.cmd)) {
+    return true;
+  }
+
+  return false;
+};
+
+Consolar.prototype.requireArgs = function(requiredArgs) {
+  var self     = this;
+  var notFound = [];
+
+  if (!lodash.isEmpty(requiredArgs)) {
+    lodash.forEach(requiredArgs, function(arg, key) {
+      try {
+        self.namedArgs[arg] = self.args[key];
+      } catch (e) {
+        notFound.push(arg);
+      }
+    });
+  }
+
+  if (!lodash.isEmpty(notFound)) {
+    console.log(chalk.red('Not enough arguments.'));
+
+    lodash.forEach(notFound, function(arg, key) {
+      console.log(chalk.yellow(arg));
+    });
+
+    this.renderCommandHelp();
+  }
+};
+
+Consolar.prototype.requireOpts = function(which) {
+  which = !lodash.isArray(which) ? which : [];
+
+  var self     = this;
+  var notFound = [];
+
+  if (!lodash.isEmpty(which)) {
+
+    lodash.forEach(which, function(opt, key) {
+      if (!lodash.has(self.opts, opt)) {
+        notFound.push(opt);
+      }
+    });
+  }
+
+  if (!lodash.isEmpty(notFound)) {
+    console.log(chalk.red('Options missing.'));
+
+    lodash.forEach(notFound, function(opt, key) {
+      console.log(chalk.yellow(opt));
+    });
+
+    this.renderCommandHelp();
   }
 };
 
@@ -138,30 +248,6 @@ Consolar.prototype.normalizeCommand = function(cmd) {
   }
 
   return path.replace(this.commandPrefix, '');
-};
-
-Consolar.prototype.shouldShowHelp = function() {
-  if (lodash.isEmpty(this.consoleInput)) {
-    return true;
-  }
-
-  if (lodash.has(this.opts, 'h') || lodash.has(this.opts, 'help') && lodash.isEmpty(this.cmd)) {
-    return true;
-  }
-
-  return false;
-};
-
-Consolar.prototype.shouldShowCommandHelp = function() {
-  if (!lodash.isEmpty(this.cmd) && (lodash.has(this.opts, 'h') || lodash.has(this.opts, 'help'))) {
-    return true;
-  }
-
-  return false;
-};
-
-Consolar.prototype.shouldShowVersion = function() {
-  return lodash.has(this.opts, 'v') || lodash.has(this.opts, 'version');
 };
 
 Consolar.prototype.step = function(msg, data) {
